7d2e92015e0b9fdbe0af02fb2159fa7cfa02df3b
willin <willin@willin.org>
2017-07-04 16:42:32 +0800

    Docsify Auto Published

--- config.js
+++ config.js
@@ -85,6 +85,7 @@ window.$docsify = {
           ads[i].insertAdjacentHTML('afterend', '<ins class="adsbygoogle" style="display:block;margin: 1.5em auto;" data-ad-client="ca-pub-5059418763237956" data-ad-slot="9518721243" data-ad-format="auto"></ins>');
           (adsbygoogle = window.adsbygoogle || []).push({});
         }
+        ga('send', 'pageview', location.hash.replace('#', ''));
       });
     }
   ],
@@ -105,3 +106,10 @@ window.$docsify = {
 if (typeof navigator.serviceWorker !== 'undefined') {
   navigator.serviceWorker.register('sw.js');
 }
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-33096931-4', 'auto');
+ga('send', 'pageview');
